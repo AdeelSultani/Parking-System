e050bf903d5546ef3d5763f0275ae9754fc9efb0
Win 10 <adeelsultani68@gmail.com>
2025-10-05 11:02:47 -0700

    Initial Commit

---
+++ lib/Screens/parkview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ParkviewScreen extends StatefulWidget {
+  const ParkviewScreen({super.key});
+
+  @override
+  State<ParkviewScreen> createState() => _ParkviewScreenState();
+}
+
+class _ParkviewScreenState extends State<ParkviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      
+    );
+  }
+}
