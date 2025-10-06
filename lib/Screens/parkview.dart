35edb149f46d1cb22b81f02bca39d9dde6d44766
Win 10 <adeelsultani68@gmail.com>
2025-10-06 11:13:30 -0700

    Through Model List is fix

--- lib/Screens/parkview.dart
+++ lib/Screens/parkview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:parking_system/model/vehcile.dart';
 
 class ParkviewScreen extends StatefulWidget {
   const ParkviewScreen({super.key});
@@ -8,128 +9,178 @@ class ParkviewScreen extends StatefulWidget {
 }
 
 class _ParkviewScreenState extends State<ParkviewScreen> {
-  int total_earning=0;
+  int total_earning = 0;
   String? raioButtonValue;
-  TextEditingController reg_no=TextEditingController();
-  List<String> parkedList=[];
+  TextEditingController reg_no = TextEditingController();
+  TextEditingController searchController=TextEditingController();
+
+  List<VechicleModel> parkedList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   
-appBar: AppBar(
-  backgroundColor: Colors.green,
-  title: Text('Parking System',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-),
-body: Padding(padding: EdgeInsets.all(10),
-child: Column(
- // mainAxisAlignment: MainAxisAlignment.center,
-  //crossAxisAlignment: CrossAxisAlignment.center,
-  children: [
-    Container(
-      width: 170,
-      height: 33,
-      decoration: BoxDecoration(
-        //color: Colors.green,
-      border: Border.all(
-      color: Colors.green,  // border color
-      width: 3,             // border thickness
-    ),
-    //  borderRadius: BorderRadius.all(
-    //   Radius.circular(5),
-    // ),
-      ),
-      child: Center(
-        child: Text('Total Earning: RS ${total_earning}',style: TextStyle(fontSize: 20,fontWeight:FontWeight.bold,color: Colors.green))),
-      ),
-      SizedBox(height: 10,),
-    Card(
-      elevation: 10,
-      child: Container(
-        width: 400,
-        height: 180,
-        decoration: BoxDecoration(
-          color: Colors.white70,
-         borderRadius: BorderRadius.all(
-      Radius.circular(10),
-    ),
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          'Parking System',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
-   
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextFormField(
-              
-              controller: reg_no,
-              decoration: InputDecoration(
-                label: Text('Registration Number'),
-                border: OutlineInputBorder()
-              ),
-            ),
-          ),
-          SizedBox(height: 5,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Radio(
-                  activeColor: Colors.green,
-                value:'Car', groupValue:raioButtonValue , onChanged: (String ?val){
-              raioButtonValue=val;
-              setState(() {
-                
-              });
-              },
-              
-              ),
-Text('Car'),
-SizedBox(width: 5,),
-  Radio(
-                  activeColor: Colors.green,
-                value:'Bike', groupValue:raioButtonValue , onChanged: (String ?val){
-              raioButtonValue=val;
-              setState(() {
-                
-              });
-              },
-              
-              ),
-Text('Bike'),
-            ],
-          ),
-          SizedBox(height: 2,),
-          ElevatedButton(onPressed: (){}, 
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green
-          ),
-          child: Text('Park in',style: TextStyle(color: Colors.white),))
-        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
         
-      ),
-      ),
-    ),SizedBox(height: 10,),
-    TextFormField(
-            
-              controller: reg_no,
-              decoration: InputDecoration(
-                prefix:Icon(Icons.search),
-                label: Text('Search By Registration Number'),
-                border: OutlineInputBorder()
+          children: [
+            Container(
+              width: 200,
+              height: 33,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.green,
+                  width: 3,
+                ),
+               
+              ),
+              child: Center(
+                child: Text(
+                  'Total Earning: RS $total_earning',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
               ),
             ),
-            SizedBox(height: 8,),
-            parkedList.length==0?Expanded(
-              child: Center(child: Text('No Vehicle Parked',style: TextStyle(fontSize: 20),)),
+            SizedBox(height: 10),
+            Card(
+              elevation: 10,
+              child: Container(
+                width: 400,
+                height: 180,
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
 
-            ):ListView.builder(
-              itemCount: parkedList.length,
-              itemBuilder: (context,index){
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: reg_no,
+                        decoration: InputDecoration(
+                          label: Text('Registration Number'),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Radio(
+                          activeColor: Colors.green,
+                          value: 'Car',
+                          groupValue: raioButtonValue,
+                          onChanged: (String? val) {
+                            raioButtonValue = val;
+                            setState(() {});
+                          },
+                        ),
+                        Text('Car'),
+                        SizedBox(width: 5),
+                        Radio(
+                          activeColor: Colors.green,
+                          value: 'Bike',
+                          groupValue: raioButtonValue,
+                          onChanged: (String? val) {
+                            raioButtonValue = val;
+                            setState(() {});
+                          },
+                        ),
+                        Text('Bike'),
+                      ],
+                    ),
+                    SizedBox(height: 2),
+                    ElevatedButton(
+                      onPressed: () {
+                          Icon radioicon;
+                        var text=reg_no.text.toString();
+                         if(raioButtonValue=='Bike'){
+                          radioicon=Icon(Icons.pedal_bike);
+                        }else{
+                          radioicon=Icon(Icons.car_crash);
+                        }
+                      VechicleModel  obj=VechicleModel(title:text , iconbtn: radioicon);
+                       
 
-            }),
+parkedList.add(obj);
+setState(() {
+  
+});
 
-  ],
-),
-),
-
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                      ),
+                      child: Text(
+                        'Park in',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: searchController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                labelText: 'Search By Registration Number',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 8),
+            parkedList.length == 0
+                ? Expanded(
+                    child: Center(
+                      child: Text(
+                        'No Vehicle Parked',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  )
+                : Expanded(
+                  child: ListView.builder(
+                      itemCount: parkedList.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 5,
+                          child: ListTile(
+                          leading: parkedList[index].iconbtn,
+                            title: Text(parkedList[index].title),
+                            trailing: ElevatedButton(onPressed: (){
+                            if(parkedList[index].iconbtn==Icon(Icons.car_crash)){
+                              total_earning+=100;
+                            }else{
+                              total_earning+=50;
+                            }
+                            parkedList.removeAt(index);
+setState(() {
+  
+});
+                            }, child: Text('Park')),
+                                            
+                          ),
+                        );
+                        
+                      },
+                    ),
+                ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/model/vehcile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class VechicleModel{
+
+  String title;
+  Icon iconbtn;
+
+  VechicleModel({required this.title,required this.iconbtn});
+}
